fb6b56c5f1ed74159f1e4efcbb4107d71de9e708
elvenbright <elvenbright@gmail.com>
2018-03-04 01:53:11 +0300

    update2

---
+++ Homework1/ComponentShop.js
@@ -0,0 +1,39 @@
+var Block = React.createClass({
+
+displayName: 'Block',
+
+
+propTypes: {
+	PhoneCat:React.PropTypes.arrayOf(
+		React.PropTypes.shape({
+		phoneModel: React.PropTypes.string.isRequired,
+		code: React.PropTypes.number.isRequired,
+		pic: React.PropTypes.string.isRequired,
+		available: React.PropTypes.number.isRequired,
+		price: React.PropTypes.number.isRequired,
+		})
+	)
+
+},
+
+render: function() {
+	var outputArr=[];
+	var arr = this.props.PhoneCat
+	arr.forEach(function(item, i, arr) {
+		var some=arr[i];
+		var Arr=
+		React.DOM.div({key:some.code,className:'PhoneType'},
+			React.DOM.span({className:'PhoneModel'},some.phoneModel),
+			React.DOM.span({className:'Descr'},some.descrip),
+			React.DOM.img({className:'picPhone',src: some.pic}),
+			React.DOM.span({className:'Available'},some.available),
+			React.DOM.span({className:'Price'},some.price),
+		);
+		outputArr.push(Arr);
+	});
+
+	return React.DOM.div( {className:'CatalogBlock'}, 
+		React.DOM.div( {className:'Table'}, outputArr ),
+	);
+},
+});
