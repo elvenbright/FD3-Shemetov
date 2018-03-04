9d5f80f2342c0c998f1294c0cc8a1d61574f906c
elvenbright <elvenbright@gmail.com>
2018-03-04 17:39:34 +0300

    update

--- Homework1/ComponentShop.js
+++ Homework1/ComponentShop.js
@@ -20,14 +20,17 @@ render: function() {
 	var outputArr=[];
 	var arr = this.props.PhoneCat
 	arr.forEach(function(item, i, arr) {
-		var some=arr[i];
+		//var some=arr[i];
+		console.log(arr[i]);
+		console.log(item); //right
+		console.log(item[i]);
 		var Arr=
-		React.DOM.div({key:some.code,className:'PhoneType'},
-			React.DOM.span({className:'PhoneModel'},some.phoneModel),
-			React.DOM.span({className:'Descr'},some.descrip),
-			React.DOM.img({className:'picPhone',src: some.pic}),
-			React.DOM.span({className:'Available'},some.available),
-			React.DOM.span({className:'Price'},some.price),
+		React.DOM.div({key:item.code,className:'PhoneType'},
+			React.DOM.span({className:'PhoneModel'},item.phoneModel),
+			React.DOM.span({className:'Descr'},item.descrip),
+			React.DOM.img({className:'picPhone',src: item.pic}),
+			React.DOM.span({className:'Available'},item.available),
+			React.DOM.span({className:'Price'},item.price),
 		);
 		outputArr.push(Arr);
 	});
